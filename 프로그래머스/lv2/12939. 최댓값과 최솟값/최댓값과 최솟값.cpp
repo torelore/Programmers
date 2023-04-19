e62404342979baeeb73ddb82a41311d1abd45f75
torelore <87071807+torelore@users.noreply.github.com>
2023-04-19 22:18:48 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.04 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 string solution(string s) {
 	string answer = s;
-
+	
 	vector<int> v;
 
 	int idx = 0;
@@ -24,24 +24,7 @@ string solution(string s) {
 			break;
 	}
 
-	// Bubble
-	{
-		size_t countMax = v.size();
-
-		for (size_t s1 = 0; s1 < v.size(); s1++) {
-			size_t max = v.size() - 1;
-
-			for (size_t s = 0; s < max; s++) {
-				int num1 = v[s];
-				int num2 = v[s + 1];
-
-				if (num1 > num2) {
-					v[s + 1] = num1;
-					v[s] = num2;
-				}
-			}
-		}
-	}
+	sort(v.begin(), v.end());
 
 	answer = to_string(v.front()) + " " + to_string(v.back());
 
