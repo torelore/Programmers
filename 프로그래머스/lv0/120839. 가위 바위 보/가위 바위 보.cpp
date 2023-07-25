8c94f266947d7a633e77fb1b4c0741e8e9a58f1f
torelore <87071807+torelore@users.noreply.github.com>
2023-07-25 12:08:02 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string rsp) {
+    string answer = "";
+    
+    for(int i = 0 ; i < rsp.size(); i++){
+        if(rsp[i] == '0'){
+            answer += '5';
+        }
+        
+        if(rsp[i] == '2'){
+            answer += '0';
+        }
+        
+        if(rsp[i] == '5'){
+            answer += '2';
+        }
+    }
+    
+    return answer;
+}
