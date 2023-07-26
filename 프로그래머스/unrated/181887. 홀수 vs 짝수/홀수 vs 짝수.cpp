45f01322f417a7351395c310828b73000a49d472
torelore <87071807+torelore@users.noreply.github.com>
2023-07-26 16:00:01 +0900

    [unrated] Title: 홀수 vs 짝수, Time: 0.01 ms, Memory: 3.69 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = 0;
+    
+    int odd = 0;
+    int even = 0;
+    
+    for(int i = 0 ; i < num_list.size(); i++){
+        if(i % 2 == 0){
+            even += num_list[i];
+        }
+        if(i % 2 == 1){
+            odd += num_list[i];
+        }
+    }
+    
+    return odd > even ? odd : even;
+}
