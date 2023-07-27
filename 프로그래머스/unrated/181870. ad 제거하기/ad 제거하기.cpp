3f62938bfe8eeed144e6c1b4b15c7b31812db932
torelore <87071807+torelore@users.noreply.github.com>
2023-07-27 16:46:30 +0900

    [unrated] Title: ad 제거하기, Time: 0.22 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(vector<string> strArr) {
+    vector<string> answer;
+    
+    for(string str : strArr){
+        if(str.find("ad") != string::npos){
+            continue;
+        }
+        
+        answer.push_back(str);
+    }
+    
+    
+    return answer;
+}
