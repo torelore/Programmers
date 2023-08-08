ba649bae326782c3fb0b6d1907b28363bb20dfe3
torelore <87071807+torelore@users.noreply.github.com>
2023-08-08 09:49:01 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 0.33 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.cpp
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.cpp
@@ -7,24 +7,25 @@ vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
     vector<int> answer;
     
     for(vector<int> query : queries){
+        int idx = 0;
+        
         int min = 1000001;
         
-        for(int i = 0; i < arr.size(); i++){
-            if(query[0] <= i && query[1] >= i){
-                if(arr[i] > query[2]){
-                    min = min < arr[i] ? min : arr[i];
-                }
+        for(int num : arr){
+            
+            if(query[0] <= idx && query[1] >= idx){
+                if(query[2] < arr[idx])
+                    min = min < num ? min : num;
             }
         }
-        
-        if(min <= 1000000){
+            
+        if(min <= 1000000)
             answer.push_back(min);
-        }
         else{
             answer.push_back(-1);
         }
-        
-        
+
+        idx++;
     }
     
     
