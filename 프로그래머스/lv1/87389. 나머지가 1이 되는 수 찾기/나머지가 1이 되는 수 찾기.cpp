f8133a63ad8d697630406e2fabaa90148f1d0de1
torelore <87071807+torelore@users.noreply.github.com>
2023-08-11 09:44:00 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 1;
+    
+    while(true){
+        if(n % answer == 1)
+            break;
+        
+        answer++;
+    }
+    
+    return answer;
+}
